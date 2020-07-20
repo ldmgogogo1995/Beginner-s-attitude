0961707489f1eef1923b2250b5104f61b1d12a2e
luodaming <775495877@qq.com>
2020-07-21 00:04:58 +0800

    学习学习冲冲冲

--- NodeJs/day04（express）/2-server-api.js
+++ NodeJs/day04（express）/2-server-api.js
@@ -1,8 +1,26 @@
 const express = require('express')
+const bodyUser = require('body-parser')
+const bodyParser = require('body-parser')
 const app = express()
     //查看用户列表接口
+    //创建中间件
+app.use('/user', (req, res, next) => {
+        //拦截 对user 的请求统一处理
+        console.log('我是全局中间件')
+        next()
+    })
+    //可以接受到x-www-form-urlencode格式的数据
+app.use(bodyParser.urlencoded({
+        extended: false
+    }))
+    //可以接受到json格式传递的数据
+app.use(bodyParser.json({
+    extended: false
+}))
+
 app.get('/user/list', (req, res) => {
-        // 用户数据
+        console.log(req, 'req')
+            // 用户数据
         let data = [{
             id: 1,
             username: 'express',
@@ -20,9 +38,10 @@ app.get('/user/list', (req, res) => {
             'data': {
                 'data': data
             }
-
         })
 
+        //req.query属性来接受传递的参数
+        console.log(req.query, '传递的参数')
     })
     // 用户注册接口
 app.post('/user/registry', (req, res) => {
@@ -33,15 +52,49 @@ app.post('/user/registry', (req, res) => {
         })
     })
     //用户登录接口
+    //接受post的参数
 app.post('/user/login', (req, res) => {
-    console.log('登录接口')
-    res.send({
-        error: 0,
-        msg: '登录成功'
+        //要获取post 的参数需要使用body-parser模块，配置后传递数据通过req.body拿到
+        const {
+            userName,
+            passWord
+        } = req.body
+        console.log(userName, passWord, '登录接口')
+        res.send({
+            error: 0,
+            msg: '登录成功'
+        })
     })
-})
+    //获取用户详情接口
+app.get('/user/detail', (req, res) => {
+        const id = req.query.userId
+        console.log(id, '传递的请求参数')
+        if (!id) {
+            res.send({
+                error: 1,
+                message: '请传递必要的参数'
+            })
+            return
+        }
+        const info = [{
+            id: 1,
+            username: 'express',
+            age: 22,
+            sex: 1
+        }, {
+            id: 2,
+            username: 'koa',
+            age: 23,
+            sex: 2
+        }].find(user => user.id == id)
+        console.log(info, 'info')
+        res.send({
+            error: 0,
+            data: info || '没有找到相关人员'
+        })
 
-//监听端口
+    })
+    //监听端口
 app.listen(8080, () => {
     console.log('连接成功...')
 })
