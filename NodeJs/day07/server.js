f764f5093107476d1765fdc5b25f705f4fbf8fe5
luodaming <775495877@qq.com>
2020-09-12 09:18:55 +0800

    feat

---
+++ NodeJs/day07/server.js
@@ -0,0 +1,5 @@
+ const express = require('express')
+ const app = express()
+ const port = 3000
+
+ app.listen(port, () => console.log('start on 3000'))
---
+++ TypeScript/function/function-1.ts
@@ -0,0 +1,12 @@
+function addFun(x: number, y: number): number {
+  return x + y;
+}
+let myAdd = function (x: number, y: number): number {
+  return x + y;
+};
+let myApp: (x: number, y: number) => number = function (
+  x: number,
+  y: number
+): number {
+  return x + y;
+};
