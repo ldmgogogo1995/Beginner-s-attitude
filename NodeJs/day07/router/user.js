ad3f3c7f3e1e8f0fa8accff3dcd8e77024ac1160
luodaming <775495877@qq.com>
2020-09-18 00:44:52 +0800

    bodyParser gogo

--- NodeJs/day07/router/user.js
+++ NodeJs/day07/router/user.js
@@ -35,4 +35,5 @@ router.put('/edit', (req, res) => {
     })
 
 })
+
 module.exports = router
--- NodeJs/day07/server.js
+++ NodeJs/day07/server.js
@@ -3,13 +3,14 @@
  const port = 3000
  const userRoter = require('./router/user')
  const bodyParser = require('body-parser')
- app.use('/user', userRoter) //绑定路由规则
-     //可以接受到x-www-form-urlencode格式的数据
  app.use(bodyParser.urlencoded({
-         extends: false
+         extended: false
      }))
      //可以接受json格式的数据
  app.use(bodyParser.json({
-     extends: false
+     extended: false
  }))
+ app.use('/user', userRoter) //绑定路由规则
+     //可以接受到x-www-form-urlencode格式的数据
+
  app.listen(port, () => console.log('start on 3000...'))
