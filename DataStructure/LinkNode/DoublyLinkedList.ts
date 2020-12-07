00d7b5529f50db2759d24e2aed4b34f24a62dc40
luodaming <775495877@qq.com>
2020-12-08 00:55:18 +0800

    feat

--- DataStructure/LinkNode/DoublyLinkedList.ts
+++ DataStructure/LinkNode/DoublyLinkedList.ts
@@ -1,19 +1,22 @@
-/** 
+/**
  * 双向链表：原本的链表的基础上变成两个链接，一个指向上个元素，一个指向下一个元素
-*/
+ *
+ * @format
+ */
+
 const LinkListtools = require('./index')
 const LinkNodetools = require('./LinkNodeCil')
 const defaultEquals = require('./utils')
 
 class DoublyNode extends LinkNodetools {
-  constructor(element, next, prev) {
-    super(element, next);
-    this.prev = prev
-  }
+    constructor(element, next, prev) {
+        super(element, next)
+        this.prev = prev
+    }
 }
 class DoubliLinkedList extends LinkListtools {
-  constructor(equalsFn = defaultEquals) {
-    super(equalsFn);
-    this.tail = undefined//新增的
-  }
+    constructor(equalsFn = defaultEquals) {
+        super(equalsFn)
+        this.tail = undefined //新增的
+    }
 }
